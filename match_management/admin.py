91b45c6c10b49a9b1986c616cacc7b5746fd4669
Adil Shamuratov <iswearican_1@outlook.com>
2023-03-08 00:46:28 +0600

    clear

--- match_management/admin.py
+++ match_management/admin.py
@@ -16,6 +16,7 @@ class ChildMatchedProductAdmin(ModelView, model=ChildMatchedProduct):
     page_size = 100
     column_list = ['nm_id', 'parent_nm_id', 'title', 'price', 'vendor_name', 'vendor_code', 'is_correct']
     column_searchable_list = ['parent_nm_id']
+    column_default_sort = [(ChildMatchedProduct.price, True)]
 
 
 class BrandAdmin(ModelView, model=Brand):
