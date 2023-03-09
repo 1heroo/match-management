2591eb827ae7c8a3c44c6bc298ea9a69160c1982
Adil Shamuratov <iswearican_1@outlook.com>
2023-03-09 22:55:36 +0600

    added characteristics output system

--- comparison_management/routes.py
+++ comparison_management/routes.py
@@ -1,4 +1,8 @@
+import io
+
+import pandas as pd
 from fastapi import APIRouter
+from starlette.responses import StreamingResponse
 
 from comparison_management.services import CMServices
 
@@ -7,7 +11,19 @@ router = APIRouter(prefix='/compare-management', tags=['compare-management'])
 cm_services = CMServices()
 
 
-# @router.get('/get-characteristics/{article_wb}/')
-# async def get_characteristics(article_wb: int):
-#     characteristics = await cm_services.chars_management(article_wb=article_wb)
+@router.get('/get-characteristics/{article_wb}/')
+async def get_characteristics(article_wb: int):
+    characteristics = await cm_services.chars_management(article_wb=article_wb)
+
+    df = pd.DataFrame(characteristics).T
+
+    output = io.BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer)
+    writer.save()
+
+    return StreamingResponse(io.BytesIO(output.getvalue()),
+                             media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                             headers={'Content-Disposition': f'attachment; filename="characteristics.xlsx"'})
+
 
--- comparison_management/utils.py
+++ comparison_management/utils.py
@@ -1,12 +1,71 @@
+import asyncio
+import json
+from match_management.utils import make_head, make_tail
+import aiohttp
+from comparison_management.whs import whs as wh_dicts
 
 
 class CMUtils:
+
+    @staticmethod
+    async def make_get_request(url, headers):
+        async with aiohttp.ClientSession(trust_env=True, headers=headers) as session:
+            response = await session.get(url=url)
+            if response.status == 200:
+                return json.loads(await response.text())
+
+    async def get_product_data(self, article):
+        card_url = make_head(int(article)) + make_tail(str(article), 'ru/card.json')
+        obj = {}
+        card = await self.make_get_request(url=card_url, headers={})
+
+        detail_url = f'https://card.wb.ru/cards/detail?spp=27&regions=80,64,38,4,83,33,68,70,69,30,86,75,40,1,22,66,31,48,110,71&pricemarginCoeff=1.0&reg=1&appType=1&emp=0&locale=ru&lang=ru&curr=rub&couponsGeo=12,3,18,15,21&sppFixGeo=4&dest=-455203&nm={article}'
+        detail = await self.make_get_request(detail_url, headers={})
+
+        if detail:
+            detail = detail['data']['products']
+        else:
+            detail = {}
+
+        seller_url = make_head(int(article)) + make_tail(str(article), 'sellers.json')
+        seller_data = await self.make_get_request(seller_url, headers={})
+
+        qnt_url = f'https://product-order-qnt.wildberries.ru/by-nm/?nm={article}'
+        qnt = await self.make_get_request(url=qnt_url, headers={})
+
+        obj.update({
+            'card': card if card else {},
+            'detail': detail[0] if detail else {},
+            'seller': seller_data if seller_data else {},
+            'qnt':  qnt if qnt else {}
+        })
+        return obj
+
+    async def get_detail_by_nms(self, nms):
+        output_data = []
+        tasks = []
+        count = 1
+
+        for nm in nms:
+            task = asyncio.create_task(self.get_product_data(article=nm))
+            tasks.append(task)
+            count += 1
+
+            if count % 50 == 0:
+                print(count, 'product data')
+                output_data += await asyncio.gather(*tasks, return_exceptions=True)
+                tasks = []
+
+        output_data += await asyncio.gather(*tasks)
+        output_data = [item for item in output_data if not isinstance(item, Exception) and item]
+        return output_data
 
     @staticmethod
     async def prepare_chars_for_output(products):
         output_data = []
 
         for product in products:
+            article = product['card'].get('nm_id')
 
             salePriceU = product['detail'].get('salePriceU')
             priceU = product['detail'].get('priceU')
@@ -14,10 +73,18 @@ class CMUtils:
             if salePriceU:
                 salePriceU //= 100
             if priceU:
-                product //= 100
+                priceU //= 100
+
+            qnt = product['qnt']
+            if qnt:
+                qnt = qnt[0].get('qnt')
+
+            sale_sum = None
+            if qnt and salePriceU:
+                sale_sum = qnt * salePriceU
 
             obj = {
-                'article wb': product['card'].get('nm_id'),
+                'article wb': article,
                 'supplier_id': product['detail'].get('supplierId'),
                 'Наименование Поставщика': product['seller'].get('supplierName'),
                 'Наименование': product['card'].get('name'),
@@ -25,24 +92,51 @@ class CMUtils:
                 'Подкатегория(subj_name)': product['card'].get('subj_name', None),
                 # 'Вид Категории(imt_name)': card.get('imt_name', None),
                 'Вендор Код (vendor_code)': product['card'].get('vendor_code', None),
+                'Бренд': product['detail'].get('brand'),
+                'id бренда': product['detail'].get('brandId'),
                 # 'Цвет (color)': card.get('nm_colors_names', None),
                 # 'sale': data.get('sale', None),
-
-
-                # 'Цена': priceU,
-                # 'Цена со скидкой': salePriceU,
-                # 'Kупили': qnt,
-                # 'Сумма продаж': int(int(data["salePriceU"] / 100) * qnt),
+                'Цена': priceU,
+                'Цена со скидкой': salePriceU,
+                'Kупили': qnt,
+                'Примерная сумма продаж': sale_sum,
                 # 'Начало продажи': date_from,
-                # 'Бренд': data.get('brand', None),
-                # 'id бренда': int(data.get('brandId', False)),
-                # 'season': card.get('season', None),
-                # 'Фото(pics)': int(data.get('pics', False)),
-                # 'Пол(kinds)': card.get('kinds', None),
-                # 'feedbacks': data.get('feedbacks', None),
-                # 'rating': data.get('rating', None),
-                # 'compositions': [item.get('name') for item in compositions] if compositions is not None else None,
-                # 'Ссылка': f'https://www.wildberries.ru/catalog/{article}/detail.aspx?targetUrl=BP'
+
+                'Фото(pics)': product['detail'].get('pics'),
+                'Описание': product['card'].get('description'),
+                'feedbacks': product['detail'].get('feedbacks'),
+                'rating': product['detail'].get('rating'),
+                'compositions': [item.get('name') for item in product['card'].get('compositions', [])],
+                'Ссылка': f'https://www.wildberries.ru/catalog/{article}/detail.aspx?targetUrl=BP'
             }
 
+            option_list = ['Ширина упаковки', 'Высота упаковки', 'Длина упаковки', 'Страна производства', 'ТНВЭД']
+            options = product['card'].get('options', [])
+
+            for option in options:
+                name = option['name']
+                if name in option_list:
+                    obj.update({name: option['value']})
+
+            sizes = product['detail'].get('sizes')
+
+            wh_dict = {}
+            for size in sizes:
+                stocks = size.get('stocks')
+                for stock in stocks:
+                    wh = stock.get('wh')
+                    wh_name = wh_dicts.get(int(wh))
+                    qty = stock.get('qty')
+
+                    if wh_dict.get(wh_name, None):
+                        wh_dict[wh_name] += qty
+                    else:
+                        wh_dict[wh_name] = qty
+
+            obj.update({
+                'FBO': sum([value for key, value in wh_dict.items() if 'продавца' not in key]),
+                'FBS': sum([value for key, value in wh_dict.items() if 'продавца' in key])
+            })
+            obj.update(wh_dict)
+            output_data.append(obj)
         return output_data
