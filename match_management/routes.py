491a0e42e916e54db8e73a01b1fa1fbfcaa9f00f
Adil Shamuratov <iswearican_1@outlook.com>
2023-03-23 00:24:48 +0600

    little changes in filtering levels

--- match_management/routes.py
+++ match_management/routes.py
@@ -45,6 +45,7 @@ async def launch_matching_with_files():
 async def main(file: bytes = File()):
     nms = [product.nm_id for product in await product_queries.fetch_all()]
     products = await match_utils.get_detail_by_nms(nms=nms)
+    print(len(products))
 
     df = pd.read_excel(file)
     article_column = df['Артикул WB'].name
